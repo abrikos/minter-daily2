e4c3f5d9391bfe0561cae6b41678fab0ea05a95b
abrikos <me@abrikos.pro>
2019-06-03 21:42:16 +0900

    log txt

--- tools/cron.sh
+++ tools/cron.sh
@@ -4,19 +4,19 @@ cd /home/abrikos/minter-daily2
 
 case "${1}" in
 "minute" )
-node server/cron.js --minute >>log/minute.log
+node server/cron.js --minute >>log/minute.log.txt
 ;;
 
 "minute5" )
-node server/cron.js --minute5 >>log/minute5.log
+node server/cron.js --minute5 >>log/minute5.log.txt
 ;;
 
 "day" )
-node server/cron.js --day>>log/day.log
+node server/cron.js --day>>log/day.log.txt
 ;;
 
 "hour" )
-node server/cron.js --hour>>log/hour.log
+node server/cron.js --hour>>log/hour.log.txt
 rm package-lock.json
 git pull
 npm i
