deac5f1751da217ae8160f5fbe28d7880fcb8b01
abrikos <me@abrikos.pro>
2019-06-04 09:58:59 +0900

    pull and build recently

--- tools/cron.sh
+++ tools/cron.sh
@@ -9,6 +9,8 @@ node server/cron.js --minute >log/minute.log.txt
 
 "minute5" )
 node server/cron.js --minute5 >log/minute5.log.txt
+git pull >log/pull.log
+npm run build > log/build.log
 ;;
 
 "day" )
@@ -18,8 +20,8 @@ node server/cron.js --day>>log/day.log.txt
 "hour" )
 node server/cron.js --hour>>log/hour.log.txt
 rm package-lock.json
-git pull
+
 npm i
-npm run build
+
 ;;
 esac
