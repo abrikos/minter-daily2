e20609b9af07934d872c68b40f3ce4303f359d77
abrikos <me@abrikos.pro>
2019-06-12 15:38:46 +0900

    remove unused

--- client/lib/MinterTransactions.js
+++ client/lib/MinterTransactions.js
@@ -1,12 +1,9 @@
 const config = require("./config");
-//const api = `https://${config.mainNet}/api/v1`;
-const moment = require('moment');
 const axios = require("axios");
-const minterCore = require('minter-js-sdk');
-require('dotenv').config()
 
 
-class Minter {
+
+class MinterTransactions {
 
 
     constructor() {
@@ -51,34 +48,9 @@ class Minter {
         this.transactionsWinners = this.transactions.filter(t => t.from === address && t.message && t.message.type === this.WinnerType);
     }
 
-    async sendToWinner(game, winner, amount) {
-        await this.send(game, winner.from, amount, {message: this.config.games[game].name + ": You Win!", type: this.WinnerType, hash: winner.hash});
-    }
-
     async getLastBlock() {
         return await this.explorer('/blocks');
     }
-
-    async send(game, address, amount, message) {
-        const minterSDK = new minterCore.Minter({apiType: 'node', baseURL: this.network.apiUrl});
-
-        const txParams = new minterCore.SendTxParams({
-            privateKey: process.env[`PK_${game}_${this.config.net}`],
-            chainId: this.network.chainId,
-            address,
-            amount,
-            coinSymbol: this.network.symbol,
-            gasPrice: 1,
-            message: typeof message === "string" ? message : JSON.stringify(message),
-        });
-        //console.log('TxParams', txParams)
-        try {
-            return await minterSDK.postTx(txParams)
-        } catch (error) {
-            //const errorMessage = error.response.data ? error.response.data.error : error;
-            console.error('ERROR', error);
-        }
-    };
 
     async getTransaction(tx) {
         return await this.explorer(`/transactions/${tx}`)
@@ -100,4 +72,4 @@ class Minter {
 };
 
 
-export default new Minter()
+export default new MinterTransactions()
